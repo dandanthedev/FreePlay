c5c222349861246f369796001abc5025250e58e4
danny <20974756+daanschenkel@users.noreply.github.com>
2023-10-01 23:51:36 +0200

    Cropped logo + new landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,4 @@
-<div class="min-w-screen min-h-screen flex flex-col items-center justify-center bg">
-	<h1 class="text-2xl font-bold text-center mt-10">What is this?</h1>
-	<p class="text-xl text-center mt-5">
-		You've stumbled upon a server running FreePlay, an open-source alternative to Cloudflare/Bunny
-		Stream!
-	</p>
-	<div
-		class="flex items-center justify-center gap-4
-    "
-	>
-		<a
-			href="https://github.com/daanschenkel/FreePlay"
-			class="mt-10 blackbg whitetext p-3 rounded-md bg-black text-white hover:text-lg transition-all duration-300"
-			>GitHub</a
-		>
-		<a
-			href="/admin"
-			class="mt-10 blackbg whitetext p-3 rounded-md bg-black text-white hover:text-lg transition-all duration-300"
-			>Admin</a
-		>
-	</div>
+<div class="min-w-screen min-h-screen flex flex-col items-center justify-center bg-black">
+	<img src="/logo.svg" alt="Logo" class="w-48 mt-10" />
+	<h1 class="text-5xl font-bold text-center mt-10 text-white">FreePlay</h1>
 </div>
-
-<style>
-	.bg {
-		background-color: #f6f6f6;
-	}
-</style>
