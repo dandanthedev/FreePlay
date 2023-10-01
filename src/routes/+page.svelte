04c5e6a3e0cb7e8c13ddf223ce5f69e64fd06ae0
danny <20974756+daanschenkel@users.noreply.github.com>
2023-10-01 23:36:50 +0200

    Added landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<div class="min-w-screen min-h-screen flex flex-col items-center justify-center bg">
+	<h1 class="text-2xl font-bold text-center mt-10">What is this?</h1>
+	<p class="text-xl text-center mt-5">
+		You've stumbled upon a server running FreePlay, an open-source alternative to Cloudflare/Bunny
+		Stream!
+	</p>
+	<div
+		class="flex items-center justify-center gap-4
+    "
+	>
+		<a
+			href="https://github.com/daanschenkel/FreePlay"
+			class="mt-10 blackbg whitetext p-3 rounded-md bg-black text-white hover:text-lg transition-all duration-300"
+			>GitHub</a
+		>
+		<a
+			href="/admin"
+			class="mt-10 blackbg whitetext p-3 rounded-md bg-black text-white hover:text-lg transition-all duration-300"
+			>Admin</a
+		>
+	</div>
+</div>
+
+<style>
+	.bg {
+		background-color: #f6f6f6;
+	}
+</style>
